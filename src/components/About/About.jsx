73da7bbdc94de7c69f394765e83c8159dcf09619
Erick Danzer <edanzer@gmail.com>
2021-08-24 23:50:32 -0600

    Removed fade, added deploy script for github pages

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,5 +1,4 @@
-import React, { useContext, useState, useEffect } from 'react';
-import Fade from 'react-reveal/Fade';
+import React, { useContext } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import Title from '../Title/Title';
 import AboutImg from '../Image/AboutImg';
@@ -7,21 +6,8 @@ import PortfolioContext from '../../context/context';
 
 const About = () => {
   const { about, footer } = useContext(PortfolioContext);
-  const { img, paragraphOne, paragraphTwo, paragraphThree, resume } = about;
+  const { img, paragraphOne, paragraphTwo, paragraphThree } = about;
   const { networks } = footer;
-
-  const [isDesktop, setIsDesktop] = useState(false);
-  const [isMobile, setIsMobile] = useState(false);
-
-  useEffect(() => {
-    if (window.innerWidth > 769) {
-      setIsDesktop(true);
-      setIsMobile(false);
-    } else {
-      setIsMobile(true);
-      setIsDesktop(false);
-    }
-  }, []);
 
   return (
     <section id="about">
@@ -29,45 +15,41 @@ const About = () => {
         <Title title="About Me" />
         <Row className="about-wrapper">
           <Col md={6} sm={12}>
-            <Fade bottom duration={800} delay={400} distance="30px">
-              <div className="about-wrapper__image">
-                <AboutImg alt="profile picture" filename={img} />
-              </div>
-            </Fade>
+            <div className="about-wrapper__image">
+              <AboutImg alt="profile picture" filename={img} />
+            </div>
           </Col>
           <Col md={6} sm={12}>
-            <Fade left={isDesktop} bottom={isMobile} duration={800} delay={400} distance="30px">
-              <div className="about-wrapper__info">
-                <p className="about-wrapper__info-text">
-                  {paragraphOne ||
-                    'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi neque, ipsa animi maiores repellendu distinctioaperiam earum dolor voluptatum consequatur blanditiis inventore debitis fuga numquam voluptate architecto itaque molestiae.'}
-                </p>
-                <p className="about-wrapper__info-text">
-                  {paragraphTwo ||
-                    'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi neque, ipsa animi maiores repellendu distinctioaperiam earum dolor voluptatum consequatur blanditiis inventore debitis fuga numquam voluptate architecto itaque molestiae.'}
-                </p>
-                <p className="about-wrapper__info-text">
-                  {paragraphThree || 'Lorem ipsum dolor sit, amet consectetur adipisicing elit.'}
-                </p>
-              </div>
-              <div className="social-links">
-                {networks &&
-                  networks.map((network) => {
-                    const { id, name, url } = network;
-                    return (
-                      <a
-                        key={id}
-                        href={url || 'https://github.com/cobidev/gatsby-simplefolio'}
-                        rel="noopener noreferrer"
-                        target="_blank"
-                        aria-label={name}
-                      >
-                        <i className={`fa fa-${name || 'refresh'} fa-inverse`} />
-                      </a>
-                    );
-                  })}
-              </div>
-            </Fade>
+            <div className="about-wrapper__info">
+              <p className="about-wrapper__info-text">
+                {paragraphOne ||
+                  'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi neque, ipsa animi maiores repellendu distinctioaperiam earum dolor voluptatum consequatur blanditiis inventore debitis fuga numquam voluptate architecto itaque molestiae.'}
+              </p>
+              <p className="about-wrapper__info-text">
+                {paragraphTwo ||
+                  'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi neque, ipsa animi maiores repellendu distinctioaperiam earum dolor voluptatum consequatur blanditiis inventore debitis fuga numquam voluptate architecto itaque molestiae.'}
+              </p>
+              <p className="about-wrapper__info-text">
+                {paragraphThree || 'Lorem ipsum dolor sit, amet consectetur adipisicing elit.'}
+              </p>
+            </div>
+            <div className="social-links">
+              {networks &&
+                networks.map((network) => {
+                  const { id, name, url } = network;
+                  return (
+                    <a
+                      key={id}
+                      href={url || 'https://github.com/cobidev/gatsby-simplefolio'}
+                      rel="noopener noreferrer"
+                      target="_blank"
+                      aria-label={name}
+                    >
+                      <i className={`fa fa-${name || 'refresh'} fa-inverse`} />
+                    </a>
+                  );
+                })}
+            </div>
           </Col>
         </Row>
       </Container>
--- src/components/Title/Title.jsx
+++ src/components/Title/Title.jsx
@@ -1,12 +1,7 @@
 import React from 'react';
-import Fade from 'react-reveal/Fade';
 import PropTypes from 'prop-types';
 
-const Title = ({ title }) => (
-  <Fade bottom duration={1000} delay={300} distance="0px">
-    <h2 className="section-title">{title}</h2>
-  </Fade>
-);
+const Title = ({ title }) => <h2 className="section-title">{title}</h2>;
 
 Title.propTypes = {
   title: PropTypes.string.isRequired,
